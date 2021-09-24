2261cf609d051cac9fff0b7d6a03953e2cac7087
Reinier Hernández <sasuke.reinier@gmail.com>
2021-09-23 20:46:40 -0400

    estableciendo el puerto del servidor por variable de entorno

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const scrapping = require('./scrapping')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
